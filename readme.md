241081d722a27cf3693cfbb72d342b834ba1e931
Yairon Vargas Aguila <yaironvargas91@gmail.com>
2020-12-20 08:43:05 +0100

    [ara probar server

---
+++ readme.md
@@ -0,0 +1,9 @@
+###### Aplicacion Web Server
+
+
+prueba para montar, server
+```````
+Solo funciona el about y el home por ahora
+```````
+
+by @yairon
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/public'));
+
+
+//EXPRESS HBS ENGINE
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'yAiRon vargas aguila'
+    });
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones por el puerto ${port}`);
+});
